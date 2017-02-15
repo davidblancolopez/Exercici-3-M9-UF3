54a8fd19bb695a1daacebcfa32f7a9b78d642967
davidblancolopez <daviddbl94@gmail.com>
2017-02-15 19:18:04 +0100

    CAMBIOS

--- src/exercici/pkg3/m9/uf3/TCPClientEcho.java
+++ src/exercici/pkg3/m9/uf3/TCPClientEcho.java
@@ -1,7 +1,14 @@
 
 package exercici.pkg3.m9.uf3;
 
+import java.net.InetAddress;
+import java.net.Socket;
+
 
 public class TCPClientEcho {
+    
+    public void TCPClientEcho(){
+        
+    }
     
 }
