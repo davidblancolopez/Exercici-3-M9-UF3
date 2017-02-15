9de673226f7d8533452c5425ee3a694b2eb39a3b
davidblancolopez <daviddbl94@gmail.com>
2017-02-16 00:03:33 +0100

    Cambios

--- src/exercici/pkg3/m9/uf3/TCPClientEcho.java
+++ src/exercici/pkg3/m9/uf3/TCPClientEcho.java
@@ -1,14 +1,22 @@
 
 package exercici.pkg3.m9.uf3;
 
-import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 
 public class TCPClientEcho {
     
-    public void TCPClientEcho(){
-        
+    Socket s;
+    DataOutputStream outToServer = new DataOutputStream(s.getOutputStream());
+    BufferedReader bf = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+    public TCPClientEcho() throws IOException {
+        this.s = new Socket("localhost", 5487);
     }
+    
     
 }
